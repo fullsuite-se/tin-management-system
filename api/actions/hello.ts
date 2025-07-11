8fe0db7c3ed451ba14dee8c35d5abdd6366cb370
leo-leung-fs <leonhard.leung@getfullsuite.com>
2025-07-11 10:20:37 +0800

    removed vercel.json

--- api/actions/hello.ts
+++
@@ -1,7 +0,0 @@
-import type {VercelRequest, VercelResponse} from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    const name = req.query.name ?? 'World';
-
-    res.status(200).send(`Hello ${name} from serverless!`);
-}
