92e11ab67161a0c58564edeff70620c2b6f47d1c
leo-leung-fs <leonhard.leung@getfullsuite.com>
2025-07-14 14:28:27 +0800

    fix: CORS error in retrieveEntry.ts

--- api/actions/retrieveEntry.ts
+++ api/actions/retrieveEntry.ts
@@ -3,6 +3,14 @@ import type { TinData } from "../../api-utils/models/tinData.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function (req: VercelRequest, res: VercelResponse) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     if (req.method !== "GET") {
         return res.status(405).json({ message: "Method not allowed" });
     }
