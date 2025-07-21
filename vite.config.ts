3b5ae700885d0d0b2b98fa3ead7a76efa5b4d087
leo-leung-fs <leonhard.leung@getfullsuite.com>
2025-07-22 07:49:20 +0800

    chore: Added vercel.json
    This commit adds a json for vercel to redirect the route to /tindatabase for the purpose of using www.getbira.com/tindatabase as the url

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-// import * as path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/tindatabase/",
   plugins: [react(), tailwindcss()],
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
 })
