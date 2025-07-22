29328c38bdbb0150fb224863abb104e9ac73ebf5
leo-leung-fs <leonhard.leung@getfullsuite.com>
2025-07-22 08:15:16 +0800

    fix: project output structure
    This commit addresses an issue where the build output is mismatched to the index.html which vercel is serving

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   base: "/tindatabase/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist/tindatabase"
+  }
 })
